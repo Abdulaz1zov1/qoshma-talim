01b75dcad7b238e50e1d8e615d665275cb16b90e
Abdulaz1zov1 <abdulazizovoyatillo94@gmail.com>
2022-07-18 15:19:57 +0500

    S

---
+++ client/src/utils/data.js
@@ -0,0 +1,153 @@
+export const listData = [
+    {
+        bachelor: [
+            {
+                id: 1,
+                country: "Belarus Respublikasi",
+                universities:
+                    [
+                        {
+                            id: 11,
+                            name: "Belarus Respublikasi, Belorussiya dav\n" +
+                                "lat oziq-ovqat va kimyoviy texnologiyalar universiteti",
+                            faculties: [
+                                "Oziq-ovqat texnologiyasi (non, makaron va qandolatchilik mahsulotlari)",
+                                "Texnologik mashinalar va jihozlar (tarmoqlar boʼyicha)",
+                                "Oziq-ovqat texnologiyasi (don mahsulotlari)",
+                                "Texnologik mashinalar va jihozlar(sovutish, kriogen texnikasi va moʼʼtadillash tizimlari mashinalari hamda agregatlari)"
+                            ]
+                        },
+                        {
+                            id: 12,
+                            name: "Belarusь Respublikasi, Belorussiya davlat texnologiya universiteti",
+                            faculties: [
+                                "Texnologik mashinalar va jihozlar (tarmoqlar boʼyicha)",
+                                "Kimyoviy texnologiya (qurilish materiallar)",
+                                "Texnologik mashinalar va jihozlar (tarmoqlar boʼyicha)",
+                                "Kimyoviy texnologiya (ishlab chiqarish turlari boʼyicha)",
+                                "Texnologik jarayonlar va ishlab chiqarishni avtomatlashtirish va boshqarish (tarmoqlar boʼyicha)",
+                                "Mashinasozlik texnologiyasi, mashinasozlik ishlab chiqarishini jihozlash va avtomatlashtirish",
+                                "Neft va neftь-gazni qayta ishlash texnologiyasi",
+                                "Neft va gaz quvurlari, baza va omborlarini qurish va ulardan foydalanish"
+                            ]
+                        },
+                        {
+                            id: 13,
+                            name: "Belarus Respublikasi, Yanka Kupala nomidagi Grodno davlat universiteti",
+                            faculties: [
+                                "Menejment (innovatsion)"
+                            ]
+                        }
+                    ]
+            },
+            {
+                id: 2,
+                country: "Rossiya Federatsiyasi",
+                universities:
+                    [
+                        {
+                            id: 14,
+                            name: "Rossiya Federatsiyasi, Qozon (Volgaboʼyi) Federal universiteti",
+                            faculties: [
+                                "Mexatronika va robototexnika",
+                                "Materialshunoslik va yangi materiallar texnologiyasi (tarmoqlar boʼyicha)",
+                                "Texnologik jarayonlar va ishlab chiqarishni avtomatlashtirish va boshqarish (tarmoqlar buyicha)"
+                            ]
+                        }
+                    ]
+            },
+            {
+                id: 3,
+                country: "Latviya Respublikasi",
+                universities:
+                    [
+                        {
+                            id: 15,
+                            name: "Latviya respublikasi, Latviya tabiiy fanlar va texnologiya universiteti",
+                            faculties: [
+                                "Oziq-ovqat texnologiyasi (mahsulot turlari boʼyicha)"
+                            ]
+                        }
+                    ]
+            },
+            {
+                id: 4,
+                country: "Turkiya Respublikasi",
+                universities:
+                    [
+                        {
+                            id: 16,
+                            name: "Turkiya, OSTIM texnika universiteti",
+                            faculties: [
+                                "Materialshunoslik va yangi material texnologiyasi (tarmoqlar bo'yicha)",
+                                "Jarayonni boshqarish uchun axborot-kommunikatsiya tizimlari"
+                            ]
+                        }
+                    ]
+            }],
+        master: [
+            {
+                id: 1,
+                country: "Belarus Respublikasi",
+                universities:
+                    [
+                        {
+                            id: 17,
+                            name: "Belarus Respublikasi, \n" +
+                                "Belorussiya davlat texnologiya universiteti",
+                            faculties: [
+                                "Kimyoviy texnologiya jarayonlari va apparatlari (ishlab chiqarish turi boʼyicha)",
+                                "Yogʼochga ishlov berish texnologiyasi va yogʼochshunoslik",
+                                "Kimyoviy texnologiya(silikat va qiyin eriydigan nometall materiallar)"
+                            ]
+                        },
+                        {
+                            id: 18,
+                            name: "Belarus Respublikasi, Belorussiya davlat oziq-ovqat va kimyoviy texnologiya universiteti",
+                            faculties: [
+                                "Oziq-ovqat mahsulotlarini ishlab chiqarish va qayta ishlash texnologiyasi (mahsulot turlari boʼyicha)",
+                                "Oziq-ovqat sanoati mashinalari va agregatlari"
+                            ]
+                        },
+                        {
+                            id: 19,
+                            name: "Belarus Respublikasi, Polotsk davlat ",
+                            faculties: [
+                                "Neft va gazni qayta ishlash va kimyoviy texnologiyasi",
+                                "Neft va gaz quvurlari, baza va omborlarini qurish va ulardan foydalanish"
+                            ]
+                        }
+                    ]
+            },
+            {
+                id: 2,
+                country: "Turkiya Respublikasi",
+                universities:
+                    [
+                        {
+                            id: 20,
+                            name: "Turkiya, OSTIM texnika universiteti",
+                            faculties: [
+                                "Texnologik jarayonlar\n" +
+                                "axborot-kommunikatsiya tizimlarini boshqarish"
+                            ]
+                        }
+                    ]
+            },
+            {
+                id: 3,
+                country: "Vengriya Respublikasi",
+                universities:
+                    [
+                        {
+                            id: 21,
+                            name: "Vengriya, Debretsen universiteti ",
+                            faculties: [
+                                "Oziq-ovqat xavfsizligi va sifati"
+                            ]
+                        }
+                    ]
+            }
+        ]
+    }
+]
